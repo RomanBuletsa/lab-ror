f6e8da295438be3d1f269dd0ec1279155d702cf3
RomanBuletsa <r.buletsa@gmail.com>
2017-10-13 14:08:02 +0300

    add pg for production

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171006121959) do
+ActiveRecord::Schema.define(version: 20171013082139) do
 
   create_table "users", force: :cascade do |t|
     t.string "firstname"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 20171006121959) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "gender"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
